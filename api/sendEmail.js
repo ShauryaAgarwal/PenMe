fe05296b913d37eb9f5f8e8e5d51acfd21e433d8
Shaurya Agarwal <48658455+ShauryaAgarwal@users.noreply.github.com>
2023-07-15 02:49:01 -0400

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -8,14 +8,14 @@ export default async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: 'shaurya220@gmail.com', // Your Gmail address
-      pass: 'bumble17', // Your Gmail password or application-specific password
+      user: 'davincisdesigns@gmail.com', // Your Gmail address
+      pass: 'Lint!!73', // Your Gmail password or application-specific password
     },
   });
 
   // Create the email message
   const mailOptions = {
-    from: 'shaurya220@gmail.com', // Sender email address (must be the same as the authenticated user)
+    from: 'davincisdesigns@gmail.com', // Sender email address (must be the same as the authenticated user)
     to: 'shaurya5525@gmail.com', // Recipient email address
     subject: 'New Contact Form Submission',
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
