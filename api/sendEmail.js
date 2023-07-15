59c2a9e0e3e17b290fa750d94550c3ed7767e781
Shaurya Agarwal <48658455+ShauryaAgarwal@users.noreply.github.com>
2023-07-15 02:41:27 -0400

    Update sendEmail.js

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,38 +1,32 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-async function sendEmail(req, res) {
-  // Retrieve the form data from the request body
+export default async (req, res) => {
+  // Get the form data from the request body
   const { name, email, message } = req.body;
 
-  // Create a transporter using your email service credentials
+  // Create a transporter with your email configuration
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: 'Gmail',
     auth: {
-      user: "shaurya220@gmail.com",
-      pass: "bumble17",
+      user: 'shaurya220@gmail.com', // Your Gmail address
+      pass: 'bumble17', // Your Gmail password or application-specific password
     },
   });
 
+  // Create the email message
+  const mailOptions = {
+    from: 'shaurya220@gmail.com', // Sender email address (must be the same as the authenticated user)
+    to: 'shaurya5525@gmail.com', // Recipient email address
+    subject: 'New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
   try {
     // Send the email
-    await transporter.sendMail({
-      from: "shaurya220@gmail.com",
-      to: "shaurya5525@gmail.com",
-      subject: "New Message from PenMe Contact Form",
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
-      `,
-    });
-
-    // Email sent successfully
-    res.status(200).json({ message: "Email sent successfully!" });
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Email sent successfully');
   } catch (error) {
-    // An error occurred while sending the email
-    console.error("Error sending email:", error);
-    res.status(500).json({ message: "Failed to send email." });
+    console.error('Error sending email:', error);
+    res.status(500).send('An error occurred while sending the email');
   }
-}
-
-module.exports = sendEmail;
+};
